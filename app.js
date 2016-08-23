8c02c34b86fb6de0690eff2902ddc39a8f7e28d6
brandl3s <brandlescandles@gmail.com>
2016-08-23 23:07:14 +0100

    broken again
    I noticed after the last commit links to pages were broken… removed
    javascript and such but no improvement

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
 //if this file is linked properly, then the console will print out "app.js ready to higgy jiggy"
 console.log("app.js ready to higgy jiggy");
 
-function colorToggle() {
-    var color = document.getElementById('art').style.color;
-    if (color == "red")
-        document.getElementById('art').style.color="blue";
-    else
-        document.getElementById('art').style.color="red";
-}
-document.getElementById('art').onclick = function(e){
-    colorToggle();// call the function
-};
